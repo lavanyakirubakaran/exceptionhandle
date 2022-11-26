58cfe5daffe3db8a698fc49557c191adb84b51f5
lavanyarajendran <116621788+lavanyakirubakaran@users.noreply.github.com>
2022-11-26 18:09:58 +0530

    Add files via upload

---
+++ NULLPointExceptionTest1.java
@@ -0,0 +1,10 @@
+package com.animals;
+
+public class NULLPointExceptionTest1 {
+	public static void main(String args[]) {
+		String[] s=new String[10];
+		s[5]="I AM THE BEST PLAYER OF KEYBOARD";
+		String lowercaseString=s[5].toLowerCase();
+		System.out.println(lowercaseString);}}
+
+
